db71dcf199e4f17b0388d83e71d6e5f16ee7d2dd
Ricky Nguyen <rnguyen89@gmail.com>
2018-02-12 12:53:19 -0800

    added login/signup form

--- public/js/app.js
+++ public/js/app.js
@@ -1,7 +1,6 @@
 'use strict';  
 
 
-
   function generateNewCard() {
     return `
     <div class="row">
@@ -40,4 +39,80 @@
     $('.card-title').html('testing');
   }
 
-  $(handleNewCard);
+// login
+
+function showLogin() {
+  const content = `
+  <section>
+    <form action="" class="login">
+      <h1>Login</h1>
+      <label for="email">Email</label><br>
+      <input type="text" name="email" required><br>
+      <label for="password">Password</label><br>
+      <input type="text" name="password" required><br>
+      <button class="login btn">login</button>
+      <p>demo user: ness</p>
+      <p>demo password: earthbound</p>      
+      <p class="no-accnt">Click <a href="#" class="create-accnt">here</a> to create an account.</p>
+    </form>
+  </section
+  `
+  $('main').html(content);
+};
+
+function login() {
+  $('.start-button').on('click', function(e) {
+    e.preventDefault();
+    showLogin();
+  });
+};
+
+function loginLink() {
+  $('main').on('click', ".login-accnt", function(e) {
+    e.preventDefault();
+    showLogin();
+  });
+};
+
+//signup
+function showSignup() {
+  const content = `
+  <section>
+    <form action="" class="sign-up">
+      <h1>Sign Up</h1>
+        <p>Please fill in this form to create an account</p>
+        <label for="name">First Name</label><br>
+        <input type="text" placeholder="Enter first name" name="first-name" required><br>
+        <label for="name">Last Name</label><br>
+        <input type="text" placeholder="Enter last name" name="last-name" required><br>
+        <label for="email">Email</label><br>
+        <input type="text" placeholder="Enter email" name="email" required><br>
+        <label for="password">Password</label><br>
+        <input type="text" placeholder="Enter password" name="password" required><br>
+        <label for="password-repeat">Repeat Password</label><br>
+        <input type="text" placeholder="Repeat password" name="password-repeat" required><br>
+
+        <button type="button" class="cancel-btn">Cancel</button>
+        <button type="submit" class="singup-btn">Sign Up</button>
+        <p class="has-accnt">Already have an account? <a href="#" class="login-accnt">Log in</a></p>
+  
+    </form>
+  </section>
+  `
+  $('main').html(content);
+};
+
+function signupLink() {
+  $('main').on('click', ".create-accnt", function(e) {
+    e.preventDefault();
+    showSignup();
+  });
+};
+
+  function ignition() {
+    login();
+    loginLink();
+    signupLink();
+  }
+
+  $(ignition);

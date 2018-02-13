cd52382190ee55d597c1f26eed539fce07c9193e
Ricky Nguyen <rnguyen89@gmail.com>
2018-02-12 21:16:03 -0800

    testing boards

--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -1,5 +1,31 @@
 'use strict';  
 
+function initIt(){
+
+$('#item1, #item2').bind('dragstart', function(event) {
+  event.originalEvent.dataTransfer.setData("text/plain", event.target.getAttribute('id'));
+});
+
+// bind the dragover event on the board sections
+$('#todo, #inprogress, #done').bind('dragover', function(event) {
+  event.preventDefault();
+});
+
+// bind the drop event on the board sections
+$('#todo, #inprogress, #done').bind('drop', function(event) {
+var notecard = event.originalEvent.dataTransfer.getData("text/plain");
+event.target.appendChild(document.getElementById(notecard));
+// Turn off the default behaviour
+// without this, FF will try and go to a URL with your id's name
+event.preventDefault();
+});
+}
+
+function generateTodoItem() {
+  return `
+  
+  `
+}
 
 function generateNewCard() {
     return `
@@ -37,6 +63,7 @@ function generateNewCard() {
 
   function init() {
     addCard();
+    initIt();
   }
 
   $(init);

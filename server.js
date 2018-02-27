6bb0cb1296d884c3839414550cfaefc5cc8f5dbd
Ricky Nguyen <rnguyen89@gmail.com>
2018-02-26 17:50:06 -0800

    added task and users

--- server.js
+++ server.js
@@ -1,22 +1,64 @@
 'use strict';
 
+const bodyParser = require('body-parser');
 const express = require('express');
 const morgan = require('morgan');
+const mongoose = require('mongoose');
+
+mongoose.Promise = global.Promise;
 
 const app = express();
 
+const { DATABASE_URL, PORT } = require('./config');
+
 const taskRouter = require('./task/router');
+const usersRouter = require('./users/router');
+
 
 app.use(express.static('public'));
 app.use('/task', taskRouter);
+app.use('/users', usersRouter);
+
 
 
 if (require.main === module) {
-  app.listen(process.env.PORT || 8080, function () {
-    console.info(`App listening on ${this.address().port}`);
+  runServer(DATABASE_URL, PORT);
+}
+
+let server;
+
+function runServer(databaseUrl, port=PORT) {
+  return new Promise((resolve, reject) => {
+    mongoose.connect(databaseUrl, {useMongoClient: true}, err => {
+      if(err) {
+        return reject(err);
+      }
+
+      server = app.listen(port, () => {
+        console.log(`Your app is listening on port ${port}`);
+        resolve();
+      })
+      .on('error', err => {
+        mongoose.disconnect();
+        reject(err);
+      });
+    });
   });
 }
 
-module.exports = app;
+function closeServer() {
+  return mongoose.disconnect().then(() => {
+    return new Promise((resolve, reject) => {
+      console.log('Closing server');
+      server.close(err => {
+        if(err) {
+          return reject(err);
+        }
+        resolve();
+      });
+    });
+  });
+}
 
+module.exports = { app, runServer, closeServer };
 
